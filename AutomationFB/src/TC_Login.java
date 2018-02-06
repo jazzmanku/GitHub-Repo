c5b9b15db1dee9ca05c5fdfbd42cf5df5ea475b9
jazzmanku <33076157+jazzmanku@users.noreply.github.com>
2018-02-05 21:07:10 -0500

    updates to code

--- AutomationFB/src/TC_Login.java
+++ AutomationFB/src/TC_Login.java
@@ -4,6 +4,8 @@ public class TC_Login {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First test script");
+		System.out.println("2nd line code");
+		
 	}
 
 }
